b7fa23830e01aae1769af0d5d19c8699fff32e5b
zxaacc <claudiagarcial2004@gmail.com>
2023-12-16 12:46:34 -0500

    Update for my project

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+const initialCards = [
+  {
+    name: "Beaver Dam",
+    link: "https://unsplash.com/photos/silhouette-of-trees-near-body-of-water-during-sunset-lsoogGC_5dg",
+  },
+  {
+    name: "Trillium Lake",
+    link: "https://unsplash.com/photos/purple-flowers-near-lake-and-snow-covered-mountain-during-daytime-fbW_BcWLBUw",
+  },
+  {
+    name: "Mount Reinier",
+    link: "https://unsplash.com/photos/a-view-of-a-snow-covered-mountain-from-a-distance-FbXBVwY9g7Y",
+  },
+  {
+    name: "Denali Park",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+  },
+  {
+    name: "Grand Teton",
+    link: "https://unsplash.com/photos/brown-and-white-mountains-under-blue-sky-during-daytime-0Q4vVmx_PV8",
+  },
+  {
+    name: "Bozeman",
+    link: "https://unsplash.com/photos/brown-field-near-mountains-under-white-clouds-during-daytime-cWFlYyJp2PE",
+  },
+];
+
+console.log(initialCards);
