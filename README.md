8a5e6b813dbe02fad49a3e319c2159ee2a90ced2
zxaacc <claudiagarcial2004@gmail.com>
2023-12-31 17:32:24 -0500

    Update for my project

--- README.md
+++ README.md
@@ -6,7 +6,11 @@ This project was meant for me to practice my HTML, CSS, and use Figma for the fi
 
 It uses flexbox and grid to display the items on the page, it also is meant to be supported on 3 diffrent displays 320px (Mobile), 800px (Tablet), 1280px (Laptop), these are the most common devices that could be used to access the site.
 
-The site is not functional but maybe in the future it could be improved to be.
+The site is currently being worked on to be become funtional with what I've been taught about in this sprint, it basic JavaScript so its nothing too fancy.
+
+I've had a lot of fun coding the small popup box, I built it using HTML, I used a <form> to add the name and description input fields, and placeholders to show what needs to be typed in each box. I styled it with simple CSS and positioned it with fixed positoning.
+
+I also had a lot of fun customizing the cards of the page and adding some of the places that I would love to visit in the future.
 
 Link to project https://zxaacc.github.io/se_project_aroundtheus/
 
--- scripts/index.js
+++ scripts/index.js
@@ -32,10 +32,49 @@ const profileModal = document.querySelector(".modal");
 const modalCloseButton = document.querySelector(
   "#modal__container_close-button"
 );
+const profileName = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
+const profileNameInput = document.querySelector(".modal__container_name");
+const profileDescriptionInput = document.querySelector(
+  ".modal__container_description"
+);
+const profileEditForm = profileModal.querySelector(".modal__form");
+const cardTemplate =
+  document.querySelector("#card_template").content.firstElementChild;
+const cardListElement = document.querySelector(".cards__list");
+
+function closeModal() {
+  profileModal.classList.remove("modal_opened");
+}
+
+function getCardElement(cardData) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImageElement = cardElement.querySelector(".card__image");
+  const cardTextElement = cardElement.querySelector(".card__text");
+  cardImageElement.src = cardData.link;
+  cardTextElement.alt = cardData.name;
+  cardTextElement.textContent = cardData.name;
+
+  return cardElement;
+}
 
 profileEditButton.addEventListener("click", () => {
+  profileNameInput.value = profileName.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
   profileModal.classList.add("modal_opened");
 });
 modalCloseButton.addEventListener("click", () => {
   profileModal.classList.remove("modal_opened");
+});
+
+profileEditForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  profileName.textContent = profileNameInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  closeModal();
+});
+
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+  cardListElement.append(cardElement);
 });
